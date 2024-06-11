1864e4aa7b1f67002626bb13da7f156d357b5f9d
guguzinhou <143537333+guguzinhou@users.noreply.github.com>
2024-06-11 16:20:26 -0300

    Add files via upload

---
+++ New-AI-main/script.js
@@ -0,0 +1,35 @@
+const caixaPrincipal = document.querySelector('.caixa-principal');
+const caixaPerguntas = document.querySelector('.caixa-perguntas');
+const caixaAlternativas = document.querySelector('.caixa-alternativas');
+const caixaResultados = document.querySelector('.caixa-resultados');
+const textoResultados = document.querySelector('.texto-resultados');
+
+const perguntas= [
+    { 
+    enunciado:"Porque flores viram frutas?" ,
+    alternativas: ["os frutos surgem para proteger a sementes e aumentar sua população",
+    "As flores quando se sentem desidratadas viram frutas para carregar mais suco e manter as sementes seguras por mais tempo"
+]
+
+},
+{ 
+    enunciado:"Qual a comida mais consumida no mundo?" ,
+    alternativas: ["Café",
+    "Leite"
+]
+
+},   { 
+    enunciado:"Qual a comida mais perigosa do mundo?" ,
+    alternativas: ["Peixe-baiacu",
+    "Peixe-boi-africano"
+]
+
+},   { 
+    enunciado:"pergunta 1" ,
+    alternativas: ["alternativa 1",
+    "alternativa 2"
+]
+
+},
+
+]
